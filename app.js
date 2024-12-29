5828a4cc8f558fa3589d01bf6a15c7fb8758613a
Inshirahsalam <i4525461@gmail.com>
2024-12-30 00:20:53 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,82 @@
+///1.Alerts..///
+// task 1://
+var a  =("welcome to javascript");
+alert(a);
+
+//task 2://
+var Name = prompt("What is your name?");
+alert("Hello" + " " + Name);
+
+
+/// Variables for string..///
+//  /task 1:///
+var a = prompt("first name ");
+var b = prompt(" last name");
+var c = a + b;
+alert(c);
+
+///task 2:///
+var A ="favorite hobby";
+ alert=("your favorite hobby is" + " " +A);
+
+
+///3.Variables for Numbers..///
+// task 1://
+var a =prompt("Current_year");
+var b =prompt("your Birth_Year");
+var c = a-b;
+alert(c);
+
+
+///task 2:///
+var a =prompt("length of rectangle");
+var b =prompt("width of rectangle");
+var c = a * b;
+alert("Area of rectangle is" + " " + c);
+
+ ///4.Variable Names Legal and Illegal..///
+ //task 1://
+ 
+ var a=prompt("what are legal variable names in javascript?");
+ alert(a);
+ var b=prompt("what are Illegal variable names in javascript?");
+ alert(b);
+
+
+//task 2://
+ var a =prompt("What is your age?");
+ var b =prompt("What is your friend's age?");
+ alert(" My age is " + " " + a + " and my friend's age is " + " " + b);
+
+///5.Math Expressions:Familiar Operators..///
+//task 1://
+var a =prompt(" enter first number for sum");
+var b =prompt("second number for sum");
+var c = a + b;
+alert("The sum is:"+ " " +c);
+
+//task 2://
+var a =prompt("enter first num for product");
+var b =prompt("enter second num for product");
+var c = a * b;
+alert("The product is:"+ " " +c);
+
+//task 3://
+var a=prompt("enter first num for remainder");
+var b=prompt("enter second num for remainder");
+var c= a % b;
+alert("The reaminder is:"+ ""+c);
+
+
+///Additional challenges..///
+//challege 1://
+var a=prompt("your birth year");
+var b=prompt("current-year");
+var c= a - b;
+alert("The difference is:"+" "+c);
+
+//challege 2://
+var a=prompt("enter firstnum");
+var b=prompt("enter second num");
+var c= a%b;
+alert("The dividend number is:"+ " "+ c);
